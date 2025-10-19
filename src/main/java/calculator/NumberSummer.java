bd33b6c18ca0118bfbd1657b23769376b7ada5d0
Foordle <131844504+Foordle@users.noreply.github.com>
2025-10-19 22:52:13 +0900

    fix(calc): handle negative and zero input values in NumberSummer

--- src/main/java/calculator/NumberSummer.java
+++ src/main/java/calculator/NumberSummer.java
@@ -4,7 +4,17 @@ public class NumberSummer {
     public int sum(String[] numbers) {
         int sum = 0;
         for (String number : numbers) {
-            sum += Integer.parseInt(number);
+            int num;
+            try{
+                num = Integer.parseInt(number);
+            }catch (NumberFormatException e){
+                throw new IllegalArgumentException("들어온값이 숫자가 아님");
+            }
+
+            if(num<=0){
+                throw new IllegalArgumentException("들어온값이 숫자가 음수임");
+            }
+            sum += num;
         }
         return sum;
     }
